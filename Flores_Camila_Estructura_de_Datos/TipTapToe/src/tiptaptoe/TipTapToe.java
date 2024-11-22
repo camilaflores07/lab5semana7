525dc391a131078a741bd57663b369641a4c246f
Camila Flores <camila_maria@unitec.edu>
2024-11-22 11:25:31 -0600

    Add files via upload

---
+++ Flores_Camila_Estructura_de_Datos/TipTapToe/src/tiptaptoe/TipTapToe.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tiptaptoe;
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+import static javax.swing.JOptionPane.showInputDialog;
+
+/**
+ *
+ * @author 50494
+ */
+public class TipTapToe {
+
+    private static void imprimirTablero() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private static boolean realizarMovimiento(char c, Scanner scanner) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+        char[][] tablero = new char[3][3];
+        String jugadorX;
+        String jugadorO;
+        int turno = 0;
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+            
+        JOptionPane.showInputDialog("Ingrese el nombre del juagdor x");
+        
+        JOptionPane.showInputDialog("Ingrese el nombre del jugador O:");
+        
+          boolean jugando = true;
+
+        
+        }
+        
+        
+      
+    }
+
+
+            
+
+   
+    
+
